6627eaa0dacf16741a6c87ff734b067e2bcc2b2d
Tanya Nguyen <Tanya.T.Nguyen-1@ou.edu>
2025-08-28 10:10:06 -0500

    started 3013 review

---
+++ 3013ReviewProblem/NumberGuessingGameSummary/Program.cs
@@ -0,0 +1,30 @@
+﻿//generate a random number between 1 and 5
+using System;
+
+//Datatype variable = value;
+Random rand = new Random();
+int randomNumber = rand.Next(1, 6); // generates a random number between 1 and 5
+
+int guess;
+do
+{
+    Console.WriteLine("Please guess a number between 1 and 5:");
+    string userGuess = Console.ReadLine();
+
+    guess = int.Parse(userGuess);
+    guess = Convert.ToInt32(userGuess);
+    Console.WriteLine($"You guessed: {userGuess}");
+
+    if (randomNumber == guess)
+    {
+        Console.WriteLine("Congratulations! You guessed the correctly!");
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you guessed incorrectly. Better luck next time!");
+    }
+
+} while (randomNumber != guess);
+
+Console.WriteLine("We generated the random number " + randomNumber + ".");
+
