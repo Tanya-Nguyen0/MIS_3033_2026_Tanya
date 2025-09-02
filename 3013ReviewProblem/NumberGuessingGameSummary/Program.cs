c3019bec780c0bd4e96127e362f92b6d4bb48f08
Tanya Nguyen <Tanya.T.Nguyen-1@ou.edu>
2025-09-02 12:31:13 -0500

    retry 3013 review with christy's class

--- 3013ReviewProblem/NumberGuessingGameSummary/Program.cs
+++ 3013ReviewProblem/NumberGuessingGameSummary/Program.cs
@@ -1,28 +1,62 @@
 ﻿//generate a random number between 1 and 5
 using System;
 
-//Datatype variable = value;
-Console.WriteLine("Welcome to our Game!");
-
-Console.WriteLine("Please input the lower bound value (lower values that could be guessed):");
 string answer = Console.ReadLine();
-int lowerBound = ValidateInput(answer);
-
-int ValidateInput(string input)
+int lowrerbound = int.Parse(answer);
+for (int i = 0; i < int.MaxValue; i++)
 {
-    int ConvertValue = 0;
-    while (int.TryParse(answer, out ConvertValue) == false)
+
+    Console.WriteLine("Please input the lower bound value (lower values that could be guessed):");
+    string answer = Console.ReadLine();
+    int lowerBound = int.Parse(answer);
+
+    Console.WriteLine("Please input the upper bound value (higher values that could be guessed):");
+    int upperBound = int.Parse(answer2);
+
+    //Datatype variable = value;
+    Random r = new Random();
+    var Random = r.Next(lowerBound, upperBound + 1); // generates a random number between 1 and 5
+    Console.WriteLine("Welcome to our Game!");
+    Console.WriteLine($"Generated a random umber with the value of " + Random + ".");
+    int guess;
+    int count = 0;
+
+    do
     {
-        Console.WriteLine("Invalid input. Please enter a valid integer:");
-        answer = Console.ReadLine();
-    }
-    return ConvertValue;
+        Console.WriteLine($"Congratulations, you guessed it correctly and it took " + count + "attempts.");
+
+        string userGuess = Console.ReadLine();
+        int guess = ValidateInput(userGuess);
+        Console.WriteLine($"You guessed: {userGuess}");
+        if (Random == guess)
+        {
+            Console.WriteLine("Congratulations! You guessed the correctly!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you guessed incorrectly. Better luck next time!");
+        }
+    } while (Random != guess);
+
 }
 
-Console.WriteLine("Please input the upper bound value (higher values that could be guessed):");
-string answer2 = Console.ReadLine();
-int upperBound = ValidateInput(answer2);
-for (int i = 0; i < lowerBound; i++) ;
+
+
+//int ValidateInput(string input)
+//{
+//    int ConvertValue = 0;
+//    while (int.TryParse(answer, out ConvertValue) == false)
+//    {
+//        Console.WriteLine("Invalid input. Please enter a valid integer:");
+//        answer = Console.ReadLine();
+//    }
+//    return ConvertValue;
+//}
+
+//Console.WriteLine("Please input the upper bound value (higher values that could be guessed):");
+//string answer2 = Console.ReadLine();
+//int upperBound = ValidateInput(answer2);
+//for (int i = 0; i < lowerBound; i++) ;
 
 
 
